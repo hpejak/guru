2637614a4cda4c8437060e138a0c6cbde59c3258
Pejak <pejak.hrvoje@gmail.com>
2021-06-07 23:11:46 +0200

    git test

--- guru-first-tutorial/src/main/java/hr/pejak/gurufirsttutorial/repositories/BookRepository.java
+++ guru-first-tutorial/src/main/java/hr/pejak/gurufirsttutorial/repositories/BookRepository.java
@@ -3,6 +3,6 @@ package hr.pejak.gurufirsttutorial.repositories;
 import hr.pejak.gurufirsttutorial.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
+//This is test for git
 public interface BookRepository extends CrudRepository<Book, Long> {
-
 }
